937cfbf81919454bbedd687784d09c9088cc8b54
NITHISH7000119 <nithishsound119@gmail.com>
2025-08-16 09:57:18 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // 🔑 Replace with your Supabase credentials
-const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-const SUPABASE_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://oarbjhjeampesttjhorq.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hcmJqaGplYW1wZXN0dGpob3JxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMTQ0OTIsImV4cCI6MjA3MDg5MDQ5Mn0.3VbKBEXZ29R6U8jbNqg6hXVc6--4qgWoFcEA69iHc04";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -35,3 +35,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
